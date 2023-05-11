33ab6c6fe1235bbe9d30c28492cd446d7a1a1811
unknown <sobhitakumarsahu@gmail.com>
2023-05-11 19:26:01 +0530

    first commit

---
+++ src/main/java/com/javatechie/spring/boot/docker/SpringBootDockerApplication.java
@@ -0,0 +1,20 @@
+package com.javatechie.spring.boot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Hello World";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
